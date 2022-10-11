90c4c40dc168d734e5bef9a647f4655c63cbd6cd
asofainy <anass.sofainy@gmail.com>
2022-10-11 14:25:45 +0100

    u

--- rclone/copy.sh
+++ rclone/copy.sh
@@ -2,7 +2,7 @@
 
 src=$1
 dest=$2
-param="--config=/opt/toolbox/rclone/config/rclone.conf"
+param="--config=/opt/toolbox/rclone/config/rclone.conf --progress"
 
 echo "[`date +'%d/%m/%Y-%H:%M:%S'`] Copying from $src to $dest  :"
 
