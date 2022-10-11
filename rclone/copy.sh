588900199fecdb2ed62883d6885bc22af039f932
asofainy <anass.sofainy@gmail.com>
2022-10-11 14:18:29 +0100

    u

--- rclone/copy.sh
+++ rclone/copy.sh
@@ -5,5 +5,5 @@ dest=$2
 
 echo "[`date +'%d/%m/%Y-%H:%M:%S'`] Copying from $src to $dest  :"
 
-rclone config --config=/opt/toolbox/config/rclone.conf 
+rclone config --config=/opt/toolbox/rclone/config/rclone.conf 
 rclone copy --progress $src $dest
