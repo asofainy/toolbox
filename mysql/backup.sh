d81c2be857cd6dc9f8036a8d670bf56b657b43c1
asofainy <anass.sofainy@gmail.com>
2022-10-22 20:05:49 +0100

    update

--- mysql/backup.sh
+++ mysql/backup.sh
@@ -4,10 +4,13 @@ MYSQL_PWD=$MYSQL_PWD
 host=$1
 username=$2
 database=$3
+backup_folder=$4
 file="dump_${database}_`date +'%Y%m%d_%H%M%S'`.sql.gz"
-backup_folder="/backup"
 
-mkdir -pv /backup/$app 
+mkdir -pv $backup_folder
+
 mysqldump -h $host -u $username --routines --triggers $database | gzip -c > $backup_folder/$file 
+
 echo "Dump file $file created :"
+
 ls -ltrah $backup_folder
