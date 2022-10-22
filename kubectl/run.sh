6cc320c043a7f25117c321eab3674a063f320980
asofainy <anass.sofainy@gmail.com>
2022-10-22 19:41:48 +0100

    update

--- kubectl/run.sh
+++ kubectl/run.sh
@@ -3,7 +3,10 @@
 namespace=$1
 app=$2
 container=$3
-cmd=$3
+cmd=$4
 
 pod=$(kubectl get pods -n $namespace -l app=$app --field-selector status.phase=Running | cut -f1 -d ' ' | tail -n 1 )
+
+echo "Using pod $pod"
+echo "CMD : $cmd"
 kubectl exec $pod -n $namespace -c $container -- /bin/bash -c "$cmd"
